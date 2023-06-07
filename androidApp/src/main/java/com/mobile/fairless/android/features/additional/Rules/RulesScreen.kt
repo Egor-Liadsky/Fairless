a78a3cc1a4d0190edcef118d2920864de63cbadc
egor.lyadsky <igeg91999@gmail.com>
2023-06-08 01:06:35 +0300

    Сделал экран "Правила поведения"

--- androidApp/src/main/java/com/mobile/fairless/android/features/additional/Rules/RulesScreen.kt
+++ androidApp/src/main/java/com/mobile/fairless/android/features/additional/Rules/RulesScreen.kt
@@ -3,14 +3,23 @@ package com.mobile.fairless.android.features.additional.Rules
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mobile.fairless.android.R
 import com.mobile.fairless.android.di.ViewModelWrapper
 import com.mobile.fairless.android.features.additional.AboutFairLess.AboutBlockView
 import com.mobile.fairless.android.features.views.topBars.CommonTopBar
 import com.mobile.fairless.android.theme.colors
+import com.mobile.fairless.android.theme.fontQanelas
 import com.mobile.fairless.features.additional.aboutFairLess.viewModel.RulesViewModel
 import org.koin.androidx.compose.get
 import org.koin.core.qualifier.named
@@ -24,7 +33,44 @@ fun RulesScreen(viewModelWrapper: ViewModelWrapper<RulesViewModel> = get(named("
                 backClick = { viewModelWrapper.viewModel.onBackButtonClick() })
         }
 
-        AboutBlockView()
+        LazyColumn {
+                item {
+                    Column(Modifier.padding(horizontal = 16.dp)) {
+                        Text(text = stringResource(id = R.string.rule1),
+                            style = TextStyle(
+                                fontFamily = fontQanelas,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 12.sp,
+                                color = colors.black,
+                                textAlign = TextAlign.Start
+                            ), modifier = Modifier.padding(top = 16.dp)
+                        )
+
+                        Text(
+                            text = "Зарещено:",
+                            style = TextStyle(
+                                fontFamily = fontQanelas,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 16.sp,
+                                color = colors.black,
+                                textAlign = TextAlign.Start
+                            ),
+                            modifier = Modifier.padding(top = 16.dp)
+                        )
+
+                        Text(text = stringResource(id = R.string.rule2),
+                            style = TextStyle(
+                                fontFamily = fontQanelas,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 12.sp,
+                                color = colors.black,
+                                textAlign = TextAlign.Start
+                            ),
+                            modifier = Modifier.padding(top = 10.dp, bottom = 16.dp)
+                        )
+                    }
+                }
+        }
 
     }
 }
