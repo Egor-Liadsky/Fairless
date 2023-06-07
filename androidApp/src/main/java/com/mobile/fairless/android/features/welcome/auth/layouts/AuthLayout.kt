abc5cb64cd686327f471892d023625825e01931b
egor.lyadsky <igeg91999@gmail.com>
2023-06-07 23:11:18 +0300

    Добавил кнопку "назад" на экране авторизации

--- androidApp/src/main/java/com/mobile/fairless/android/features/welcome/auth/layouts/AuthLayout.kt
+++ androidApp/src/main/java/com/mobile/fairless/android/features/welcome/auth/layouts/AuthLayout.kt
@@ -10,14 +10,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mobile.fairless.android.R
 import com.mobile.fairless.android.di.ViewModelWrapper
 import com.mobile.fairless.android.features.views.buttons.CommonButton
 import com.mobile.fairless.android.features.views.buttons.CommonButtonParams
+import com.mobile.fairless.android.features.views.buttons.SquareButton
 import com.mobile.fairless.android.features.views.textFields.CommonTextField
 import com.mobile.fairless.android.theme.colors
 import com.mobile.fairless.android.theme.fontQanelas
@@ -30,70 +33,80 @@ fun AuthLayout(viewModelWrapper: ViewModelWrapper<AuthViewModel>) {
         Modifier
             .fillMaxSize()
             .background(colors.backgroundWelcome),
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val state = viewModelWrapper.viewModel.state.collectAsState()
         val focusManager = LocalFocusManager.current
 
-        Text(
-            text = "Войдите в\nсвой профиль",
-            style = TextStyle(
-                fontFamily = fontQanelas,
-                fontWeight = FontWeight.Bold,
-                fontSize = 40.sp,
-                textAlign = TextAlign.Center,
-                color = colors.black
-            ),
-            modifier = Modifier.padding(top = 100.dp)
-        )
-
-        CommonTextField(
-            modifier = Modifier.padding(top = 40.dp),
-            textState = viewModelWrapper.viewModel.state.value.email ?: "",
-            placeholder = "Введите свой логин или E-mail"
+        SquareButton(
+            modifier = Modifier.padding(start = 16.dp, top = 16.dp),
+            icon = painterResource(id = R.drawable.ic_back_button),
+            backgroundColor = colors.white,
+            iconSize = 15.dp
         ) {
-            viewModelWrapper.viewModel.emailChanged(it)
-        }
-
-        CommonTextField(
-            modifier = Modifier.padding(top = 20.dp),
-            textState = viewModelWrapper.viewModel.state.value.password ?: "",
-            placeholder = "Введите свой пароль",
-            isPassword = true
-        ) {
-            viewModelWrapper.viewModel.passwordChanged(it)
-        }
-
-        CommonButton(
-            commonButtonParams = CommonButtonParams(
-                title = "Войти",
-                titleColor = colors.white,
-                background = colors.orangeMain,
-            ),
-            isLoading = state.value.isLoading,
-            modifier = Modifier.padding(top = 20.dp)
-        ) {
-            focusManager.clearFocus()
-            viewModelWrapper.viewModel.authUser(
-                UserAuthResponse(
-                    identifier = state.value.email?.filter { !it.isWhitespace() } ?: "",
-                    password = state.value.password ?: ""
-                )
-            )
-        }
-        if (state.value.user != null) {
             viewModelWrapper.viewModel.onBackButtonClick()
         }
 
-        CommonButton(
-            commonButtonParams = CommonButtonParams(
-                title = "Регистрация",
-                titleColor = colors.black,
-                background = colors.white
-            ),
-            modifier = Modifier.padding(top = 20.dp)
-        ) {
-            viewModelWrapper.viewModel.navigateToRegister()
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Войдите в\nсвой профиль",
+                style = TextStyle(
+                    fontFamily = fontQanelas,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 40.sp,
+                    textAlign = TextAlign.Center,
+                    color = colors.black
+                ),
+                modifier = Modifier.padding(top = 80.dp)
+            )
+
+            CommonTextField(
+                modifier = Modifier.padding(top = 40.dp),
+                textState = viewModelWrapper.viewModel.state.value.email ?: "",
+                placeholder = "Введите свой логин или E-mail"
+            ) {
+                viewModelWrapper.viewModel.emailChanged(it)
+            }
+
+            CommonTextField(
+                modifier = Modifier.padding(top = 20.dp),
+                textState = viewModelWrapper.viewModel.state.value.password ?: "",
+                placeholder = "Введите свой пароль",
+                isPassword = true
+            ) {
+                viewModelWrapper.viewModel.passwordChanged(it)
+            }
+
+            CommonButton(
+                commonButtonParams = CommonButtonParams(
+                    title = "Войти",
+                    titleColor = colors.white,
+                    background = colors.orangeMain,
+                ),
+                isLoading = state.value.isLoading,
+                modifier = Modifier.padding(top = 20.dp)
+            ) {
+                focusManager.clearFocus()
+                viewModelWrapper.viewModel.authUser(
+                    UserAuthResponse(
+                        identifier = state.value.email?.filter { !it.isWhitespace() } ?: "",
+                        password = state.value.password ?: ""
+                    )
+                )
+            }
+            if (state.value.user != null) {
+                viewModelWrapper.viewModel.onBackButtonClick()
+            }
+
+            CommonButton(
+                commonButtonParams = CommonButtonParams(
+                    title = "Регистрация",
+                    titleColor = colors.black,
+                    background = colors.white
+                ),
+                modifier = Modifier.padding(top = 20.dp)
+            ) {
+                viewModelWrapper.viewModel.navigateToRegister()
+            }
         }
     }
 }
