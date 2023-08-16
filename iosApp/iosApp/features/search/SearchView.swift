169c873c96824c428f876bc1dc81fc1f1685b354
egor.lyadsky <egor.lyadsky@sebbia.org>
2023-08-16 18:47:49 +0300

    Cделал BottomBar

---
+++ iosApp/iosApp/features/search/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  iosApp
+//
+//  Created by Егор Лядский on 2023-08-16.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
